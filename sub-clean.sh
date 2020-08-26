814db4567a0afa740a64abd06454b1426c43953f
brianspilner01 <58618884+brianspilner01@users.noreply.github.com>
2020-08-27 03:36:10 +0930

    Update sub-clean.sh

--- sub-clean.sh
+++ sub-clean.sh
@@ -7,15 +7,15 @@ SUB_FILEPATH="$1"
 # check usage
 [ ! -f "$SUB_FILEPATH" ] && { echo "usage: sub-clean.sh [FILE]" ; echo "Warning: subtitle file does not exist" ; exit 1 ; }
 
-# convert any DOS formatted files to UNIX (remove carriage return line endings)
-sed -i 's/\r$//' "$SUB_FILEPATH"
-
 # lowercase list of regex (gore/magic?) that will be removed from srt
 REGEX_TO_REMOVE='opensubtitles|sub(scene|text|rip)|podnapisi|addic7ed|yify|napisy|bozxphd|sazu489|anoxmous|(br|dvd|web).?(rip|scr)|english (- )?us|sdh|srt|(sub(title)?(bed)?(s)?(fix)?|encode(d)?|correct(ed|ion(s)?)|caption(s|ed)|sync(ed|hroniz(ation|ed))?|english)(.pr(esented|oduced))?.?(by|&)|[^a-z]www\.|http|\.( )?(com|co|link|org|net|mp4|mkv|avi)([^a-z]|$)|©|™'
 
 if [[ $SUB_FILEPATH =~ \.srt$ ]] # only operate on srt files
 then
 
+        # convert any DOS formatted files to UNIX (remove carriage return line endings)
+        sed -i 's/\r$//' "$SUB_FILEPATH"
+        
         ### each record (in awk) is defined as a block of srt formatted subs (record seperator RS is essentially \n\n+, see docs), with each line of the block a seperate field .i.e.:
         # LINE NUMBER
         # TIMESTAMP --> TIMESTAMP
